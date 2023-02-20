66721d80efdd4397015608d8da6e9dde8cd6074e
Sheikhsamii <samsheikh639@gmail.com>
2023-02-20 09:51:23 +0500

    first commit

---
+++ sections/Hero.jsx
@@ -0,0 +1,20 @@
+'use client';
+import styles from '../styles';
+import { motion } from "framer-motion";
+import {slideIn, staggerContainer, textVariant} from '../utils/motion'
+const Hero = () => (
+  <section className={`${styles.yPaddings} 
+  sm:pl-16 pl-6
+  `}>
+   <motion.div
+   variants={staggerContainer}
+   initial="hidden"
+   whileInView="show"
+   viewport={{once: false, amount: 0.25}}
+   >
+
+   </motion.div>
+  </section>
+);
+
+export default Hero;
